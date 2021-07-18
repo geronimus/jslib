791a5f51286db9cddf9055b28cb114b47648b4e2
Graham Brown (Geronimus) <leo.orientis@outlook.com>
2021-07-18 23:50:22 +0200

    Fix webpack config for resolveTypeName.

--- functions/resolve-type-name/webpack.config.js
+++ functions/resolve-type-name/webpack.config.js
@@ -3,12 +3,12 @@ const ESLintPlugin = require( "eslint-webpack-plugin" );
 
 module.exports = {
   mode: "production",
-  entry: "./src/try.js",
+  entry: "./src/resolve-type-name.js",
   output: {
     path: path.resolve( __dirname, "dist" ),
-    filename: "try.js",
+    filename: "resolve-type-name.js",
     globalObject: "this",
-    library: "@geronimus/try",
+    library: "@geronimus/resolve-type-name",
     libraryTarget: "umd"
   },
   plugins: [
